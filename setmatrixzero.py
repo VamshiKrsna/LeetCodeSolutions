11d3010b710428ddc7379d6949d773f6047716e8
Vamshi Krishna <107762231+VamshiKrsna@users.noreply.github.com>
2023-06-19 20:23:53 +0530

    Update setmatrixzero.py with complexity analysis

--- setmatrixzero.py
+++ setmatrixzero.py
@@ -14,3 +14,13 @@ class Solution:
             for j in range(len(matrix[0])):
                 if i in rows or j in cols:
                     matrix[i][j] = 0
+
+# Analysis of solution :
+"""
+Time Complexity :
+firstly, there are two loops that iterate over entire matrix O(m*n) m = no. of rows, n = no. of columns
+same goes for second loop also, Overall time complexity is O(m*n)
+
+Space Complexity :
+At worst case, two sets rows and cols can store m, n values respectively ( NO DUPLICATES), so Space Complexity is O(m+n) 
+"""
